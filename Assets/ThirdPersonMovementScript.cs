d1d040070288575a83b0d52ba5a2d92707421f4f
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2022-01-27 22:16:21 +0100

    add dynamic animation blending to character

--- Assets/ThirdPersonMovementScript.cs
+++ Assets/ThirdPersonMovementScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 //to do
 //implement Running and animation
@@ -12,16 +13,23 @@ using UnityEngine;
 
 public class ThirdPersonMovementScript : MonoBehaviour
 {
+    [SerializeField]
+    private float moveSmoothTime = 0.2f;
+    private float animationSmoothTime = 0.3f; 
+
+    public CharacterController contoller;
+    public Animator animator;
+
     public Transform cam;
     public Transform groundCheck;
-    public float jumpHeight = 10;
 
     public float radius;
     public LayerMask groundMask;
     public bool isGrounded; 
-
-    public CharacterController contoller;
-    public float speed = 6f;
+    
+    public float walkSpeed = 6f;
+    public float sprintSpeed = 12f; 
+    public float jumpHeight = 10f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVel;
@@ -29,15 +37,24 @@ public class ThirdPersonMovementScript : MonoBehaviour
     public Vector3 currentVel;
     public float gravity = -9.81f;
 
-    public Animator animator;
+    private bool isJumping;
 
-    bool isWalking;
-    bool isRunning;
-    bool isJumping; 
+    private StarterAssets.StarterAssetsInputs _inputs;
+
+    private Vector2 currentInput;
+    private Vector2 smoothMoveVelocity;
+
+    private float currentBlend;
+    private float smoothBlendVel;
+
+    public float jumpAniticipationTime; 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        contoller = gameObject.GetComponent<CharacterController>(); 
+        contoller = gameObject.GetComponent<CharacterController>();
+        _inputs = gameObject.GetComponent<StarterAssets.StarterAssetsInputs>();
     }
 
     // Update is called once per frame
@@ -49,67 +66,61 @@ public class ThirdPersonMovementScript : MonoBehaviour
             currentVel.y = -2f;
         }
 
-       
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        currentVel.y -= gravity * Time.deltaTime;
-
-
-        //Jumps if character is grounded. 
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            isJumping = true;
-            //currentVel.y = Mathf.Sqrt(jumpHeight * -2 * -gravity);
-            Invoke("AddJumpForce",0.15f);
-            print(currentVel);
-        }
-        else
-            isJumping = false;
-
-        //move Character 
-        contoller.Move(currentVel * Time.deltaTime );     
-        //smooths out turns
-
-        if (direction.magnitude > 0f)
-        {
-            
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f); 
-
-
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            contoller.Move( moveDirection.normalized * speed * Time.deltaTime);
-
-            isWalking = true;
-
-
-        }
-        else
-            isWalking = false;
-
-        if (isWalking && Input.GetKey(KeyCode.LeftShift))
-        {
-            isRunning = true;
-            speed = 12;
-        }
-
-        else
-        {
-            speed = 6;
-            isRunning = false;
-        }
-           
-
-        animator.SetBool("isWalking", isWalking);
-        animator.SetBool("isRunning", isRunning);
-        animator.SetBool("isJumping", isJumping);
+        Move();
+        Jump();
+    
     }
 
     public void AddJumpForce()
     {
         currentVel.y = Mathf.Sqrt(jumpHeight * -2 * -gravity);
+    }
+
+    private void Move()
+    {
+        float speed = _inputs.sprint ? sprintSpeed : walkSpeed;
+
+        Vector2 moveInput = _inputs.move;
+        currentInput =  Vector2.SmoothDamp(currentInput, moveInput, ref smoothMoveVelocity, moveSmoothTime);
+        
+        Vector3 direction = new Vector3(currentInput.x, 0f, currentInput.y);
+       
+        if (direction.magnitude > 0f)
+        {
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            contoller.Move(moveDirection.normalized * speed * Time.deltaTime * currentInput.magnitude);
+
+            currentBlend = Mathf.SmoothDamp(currentBlend, (_inputs.move.magnitude * speed) / sprintSpeed, ref smoothBlendVel, animationSmoothTime);
+            currentBlend = Mathf.Clamp(currentBlend, 0, 1);
+            print(currentBlend); 
+            animator.SetFloat("Blend", currentBlend);
+        }
+    }
+    private void Jump()
+    {
+        currentVel.y -= gravity * Time.deltaTime;
+        contoller.Move(currentVel * Time.deltaTime);
+        if (_inputs.jump && isGrounded)
+        {
+            isJumping = true;
+            //currentVel.y = Mathf.Sqrt(jumpHeight * -2 * -gravity);
+            Invoke("AddJumpForce", 0.15f);
+            
+            print(currentVel);
+        }
+        else
+        {
+            isJumping = false;
+            _inputs.jump = false;
+        }
+        animator.SetBool("isJumping", isJumping);
+        animator.SetBool("Grounded", isGrounded);
     }
 }

249b66475de9aa9ab8bc1961a3b342a846f10a62
unknown <victoria.goette01@htl-salzburg.ac.at>
2022-03-25 15:23:41 +0100

    add new shader with ambience

--- Assets/Vici_Objects/ToonShader_Lit_Excerice/ToonShader.hlsl
+++ Assets/Vici_Objects/ToonShader_Lit_Excerice/ToonShader.hlsl
@@ -1,4 +1,4 @@
-void ToonShader_float(in float3 Normal, in float ToonRampSmoothness, in float3 ClipSpacePos, in float3 WorldPos, in float ToonRampTinting, in float4 RimTinting, in float ToonRampOffset, out float3 ToonRampOutput, out float3 Direction)
+void ToonShader_float(in float3 AmbientLight, in float3 Normal, in float ToonRampSmoothness, in float3 ClipSpacePos, in float3 WorldPos, in float ToonRampTinting, in float4 RimTinting, in float ToonRampOffset, out float3 ToonRampOutput, out float3 Direction)
 {
 #ifdef SHADERGRAPH_PREVIEW
 ToonRampOutput = float3(0.5,0.5,0);
@@ -7,6 +7,7 @@ Direction = float3(0.5,0.5,0);
 #else
 	#if SHADOWS_SCREEN
 	half4 shadowCoord = ComputeScreenPos(ClipSpacePos);
+	half3 AmbientLight = UNITY_LIGHTMODEL_AMBIENT;
 
 	#else 
 	half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
@@ -21,7 +22,7 @@ Direction = float3(0.5,0.5,0);
 	#endif
 
 	half d = dot(Normal, light.direction) * 0.5 + 0.5;
-
+	
 	half toonRamp = smoothstep(ToonRampOffset, ToonRampOffset + ToonRampSmoothness, d);
 
 	toonRamp *= light.shadowAttenuation;
